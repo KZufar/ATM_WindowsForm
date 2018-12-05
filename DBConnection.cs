8bff651d25cf9fcbdaed6661d1102b6a312c7979
Kajumov <zufarkayumov@gmail.com>
2018-12-05 09:14:23 +0300

    atm

---
+++ DBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class DBConnection
+    {
+        private string con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\zufar\OneDrive\Рабочий стол\Telegram\ATM\Accounts.mdf;Integrated Security=True;";
+       
+        public string Con
+        {
+            get
+            {
+                return con;
+            }
+        }
+    }
+}
